fdcbdd841062a5341a75cfa0b77e247a56cd4d1e
chihabMg <chihab.mg.me@gmail.com>
2022-12-25 20:19:57 +0100

    added user model tests

--- backend/accounts/tests/test_user_model.py
+++ backend/accounts/tests/test_user_model.py
@@ -1,32 +1,38 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from faker import Faker
 
-# # Create your tests here.
+# Create your tests here.
 
-# User = get_user_model()
+User = get_user_model()
+fake = Faker()
 
-# class UserTest(TestCase):
-#     user_username = "test_user"
-#     user_password = "test_password"
-#     user_email = "test_email@email.com"
 
-#     @classmethod
-#     def setUpTestData(cls) -> None:
-#         user = User(username=cls.user_username, email=cls.user_email)
-#         user.set_password(cls.user_password)
-#         user.save()
+class UserTest(TestCase):
+    user_username = fake.name()
+    user_password = fake.password()
+    user_email = fake.email()
 
-#     def test_users_count(self):
-#         self.assertEqual(User.objects.count(), 1)
+    @classmethod
+    def setUpTestData(cls) -> None:
+        user = User(username=cls.user_username, email=cls.user_email)
+        user.set_password(cls.user_password)
+        user.save()
 
-#     def test_username(self):
-#         user = User.objects.get(id=1)
-#         self.assertEqual(user.username, self.user_username)
+    def get_first_user(self):
+        return User.objects.all().first()
 
-#     def test_email(self):
-#         user = User.objects.get(id=1)
-#         self.assertEqual(user.email, self.user_email)
+    def test_users_count(self):
+        self.assertEqual(User.objects.count(), 1)
 
-#     def test_password(self):
-#         user = User.objects.get(id=1)
-#         self.assertTrue(user.check_password(self.user_password))
+    def test_username(self):
+        user = self.get_first_user()
+        self.assertEqual(user.username, self.user_username)
+
+    def test_email(self):
+        user = self.get_first_user()
+        self.assertEqual(user.email, self.user_email)
+
+    def test_password(self):
+        user = self.get_first_user()
+        self.assertTrue(user.check_password(self.user_password))
